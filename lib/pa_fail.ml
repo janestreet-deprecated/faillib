63b084509c3803d09f699b73f9a37be3169c75bf
Ben Millwood <bmillwood@janestreet.com>
2014-01-15 13:27:27 +0000

    109.60.00
    - Compatibility with warning 7 (method override)

--- lib/pa_fail.ml
+++ lib/pa_fail.ml
@@ -3,7 +3,7 @@ open Camlp4.PreCast
 let map = object
   inherit Ast.map as super
 
-  method expr : Ast.expr -> Ast.expr = function
+  method! expr : Ast.expr -> Ast.expr = function
   | Ast.ExId (loc, Ast.IdLid (_, "failwiths")) ->
     <:expr@loc< failwithp ~here:$Pa_here.ast_of_loc loc$ >>
   | e -> super#expr e
